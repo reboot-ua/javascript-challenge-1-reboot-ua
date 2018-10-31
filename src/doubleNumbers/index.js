39d8439a45e00f531b52bc79f1d924bc48f9158a
reboot-ua <reboot.if.ua@gmail.com>
2018-10-31 16:04:31 +0200

    doubleNumber

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,13 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  // your logic here.
+  const numString = num.toString();
+  if (numString.length % 2 === 0) {
+    const firstPart = numString.substr(0, numString.length / 2);
+    const lastPart = numString.substr(numString.length / 2);
+    if (firstPart === lastPart) {
+      return num;
+    }
+  }
+  return num * 2;
 };
