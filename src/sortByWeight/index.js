be81446ad6dcf2a043ff712f6b1c26289150f6ec
reboot-ua <reboot.if.ua@gmail.com>
2018-11-01 15:37:33 +0200

    doubleNumbers sortByWeight

--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable-next-line */
-export const sortByWeight = arr => {
-  // your logic here...
-};
+export const sortByWeight = arr => arr.sort((a, b) => {
+  const n = a.toString().split('').reduce((sum, i) => sum + parseInt(i, 10), 0);
+  const z = b.toString().split('').reduce((sum, i) => sum + parseInt(i, 10), 0);
+
+  return n - z;
+});
